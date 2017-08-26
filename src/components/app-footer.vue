a9c5c383b4bba21ea8880a695ee85bb17c17a4b8
Moritz Kröger <write@morkro.de>
2017-08-26 12:40:00 +0200

    Added web sharing and updated mobile layout

--- src/components/app-footer.vue
+++ src/components/app-footer.vue
@@ -3,7 +3,7 @@
     <ul class="footer-social">
       <li v-for="item of social"
         :data-message="item.label === 'clipboard' ? item.message : ''"
-        :class="{ 'show-info': showInfo }">
+        :class="{ 'show-info': item.label === 'clipboard' && showClipboardInfo }">
         <button class="btn-dark" @click="share(item)" >
           <feather-icon :type="item.icon" />
         </button>
@@ -30,13 +30,28 @@
 
     data () {
       return {
-        url: window.location.origin,
-        showInfo: false,
+        shareURL: window.location.origin,
+        showClipboardInfo: false,
         infoTimeout: 2000
       }
     },
 
     methods: {
+      // https://stackoverflow.com/a/16861050/1724106
+      getPopupDimensions (w = 800, h = 600) {
+        const { screenLeft, screenTop, innerWidth, innerHeight } = window
+        const dualScreenLeft = screenLeft || screen.left
+        const dualScreenTop = screenTop || screen.top
+        const width = innerWidth || document.documentElement.clientWidth || screen.width
+        const height = innerHeight || document.documentElement.clientHeight || screen.height
+
+        return {
+          width: w,
+          height: h,
+          left: ((width / 2) - (w / 2)) + dualScreenLeft,
+          top: ((height / 2) - (h / 2)) + dualScreenTop
+        }
+      },
       share (social) {
         switch (social.label.toLowerCase()) {
           case 'twitter': return this.shareViaTwitter(social.message)
@@ -46,22 +61,34 @@
         }
       },
       shareViaTwitter () {
-        console.log('shareViaTwitter')
+        const url = 'https://twitter.com/intent/tweet'
+        const frameName = 'Post a Tweet on Twitter'
+        const { width, height, left, top } = this.getPopupDimensions()
+        const options = `resizable,scrollbars,width=${width},height=${height},top=${top},left=${left}`
+
+        const popup = window.open(url, frameName, options)
+        if (window.focus) {
+          popup.focus()
+        }
       },
       shareViaFacebook () {
-        console.log('shareViaFacebook')
+        FB.ui({
+          method: 'share',
+          display: 'popup',
+          href: this.shareURL
+        }, response => {})
       },
       copyToClipboard (message) {
         const $textarea = document.createElement('textarea')
-        $textarea.value = this.url
+        $textarea.value = this.shareURL
         document.body.appendChild($textarea)
         $textarea.select()
 
         try {
           const successful = document.execCommand('copy')
           if (successful) {
-            this.showInfo = true
-            setTimeout(() => { this.showInfo = false }, this.infoTimeout)
+            this.showClipboardInfo = true
+            setTimeout(() => { this.showClipboardInfo = false }, this.infoTimeout)
           }
         } catch (err) {
           console.log('Oops, unable to copy')
@@ -74,10 +101,12 @@
 </script>
 
 <style lang="scss" scoped>
+  @import "~styles/animations";
   @import "~styles/colors";
   @import "~styles/layout";
 
   $social-btn-size: 40px;
+  $breakpoint: 768px;
 
   .footer {
     display: flex;
@@ -86,6 +115,10 @@
 
     ul {
       list-style: none;
+    }
+
+    @media (max-width: $breakpoint) {
+      align-items: center;
     }
   }
 
@@ -113,11 +146,23 @@
   .footer-social {
     padding: 0 $small-gap 0 0;
 
+    @media (max-width: $breakpoint) {
+      display: flex;
+      justify-content: center;
+      padding: 0;
+
+      li:not(:last-child) {
+        margin-bottom: 0;
+        margin-right: $small-gap;
+      }
+    }
+
     li {
       position: relative;
 
       &.show-info::before {
-        display: block;
+        transform: translate(-105%, -50%) scale(1);
+        opacity: 1;
       }
     }
 
@@ -126,14 +171,15 @@
       position: absolute;
       top: 50%;
       left: 0;
-      transform: translate(-105%, -50%);
+      transform: translate(-105%, -50%) scale(0);
       white-space: pre;
       padding: 10px 20px;
       border-radius: $border-radius;
       background: rgba(0, 0, 0, 0.3);
       width: auto;
       font-size: $font-size-small;
-      display: none;
+      opacity: 0;
+      transition: opacity 150ms $easeInOutQuint;
     }
 
     li:not(:last-child) {
