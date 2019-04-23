dd764adf1cdb622564b9099e29d1a44d218c5d88
Moritz Kröger <write@morkro.de>
2019-04-23 21:00:29 +0100

    :lipstick: (Layout updates on FAQ)

--- src/app/faq/components/index.vue
+++ src/app/faq/components/index.vue
@@ -1,10 +1,12 @@
 <template>
-  <section>
+  <section class="faq-page">
     <h1>{{ $t('faq.headline') }}</h1>
     <ul>
       <li v-for="question of $t('faq.questions')" :key="question.title + question.answer">
         <h2>{{ question.title }}</h2>
-        <v-markdown :source="question.answer" />
+        <div class="faq-content">
+          <v-markdown :source="question.answer" />
+        </div>
       </li>
     </ul>
   </section>
@@ -16,22 +18,34 @@
   }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
   @import "~@/styles/layout";
 
-  h1 {
-    margin-bottom: $base-gap;
-  }
+  .faq-page {
+    h1 {
+      margin-bottom: $base-gap;
+    }
 
-  ul {
-    list-style: none;
+    ul {
+      list-style: none;
 
-    li:not(:last-child) {
-      margin-bottom: $base-gap * 2;
+      li:not(:last-child) {
+        margin-bottom: $base-gap * 2;
+      }
+    }
+
+    h2 {
+      margin-bottom: $small-gap;
     }
   }
 
-  h2 {
-    margin-bottom: $small-gap;
+  .faq-content ul {
+    padding-left: 1.25em;
+    margin-top: calc(#{$base-gap} / 2);
+    list-style: circle;
+
+    li {
+      margin-bottom: calc(#{$base-gap} / 2);
+    }
   }
 </style>
