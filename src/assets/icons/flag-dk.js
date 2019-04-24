97e2f1f6bbac6877d85f111a60e27a9c091f5e12
Moritz Kröger <write@morkro.de>
2019-04-24 09:10:08 +0100

    :sparkles: Adds danish

---
+++ src/assets/icons/flag-dk.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+var icon = require('vue-svgicon')
+icon.register({
+  'flag-dk': {
+    width: 16,
+    height: 16,
+    viewBox: '0 0 370 280',
+    data: '<path pid="0" _fill="#c60c30" d="M0 0h370v280H0z"/><path pid="1" _fill="#fff" d="M120 0h40v280h-40z"/><path pid="2" _fill="#fff" d="M0 120h370v40H0z"/>'
+  }
+})
