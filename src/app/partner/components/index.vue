ad2c5c22ed48b173c89a882e1e2ea37c22a74e1d
Moritz Kröger <write@morkro.de>
2019-05-05 19:57:11 +0200

    :sparkles: Finished layout for partner page

--- src/app/partner/components/index.vue
+++ src/app/partner/components/index.vue
@@ -6,20 +6,28 @@
 
     <ul class="partners-list">
       <li v-for="partner in $t('partner.partners')" :key="partner.name">
-        <h2>
-          <a
-            :href="partner.url"
-            target="_blank"
-            rel="noopener"
+        <a
+          class="partners-list-logo"
+          :href="partner.url"
+          target="_blank"
+          rel="noopener"
+        >
+          <img
+            :src="getPartnerLogo(partner.name)"
+            :alt="partner.name"
+            :title="partner.name"
           >
+        </a>
+        <div class="partners-list-content">
+          <h2>
             {{ partner.name }}
-          </a>
-        </h2>
-        <p>{{ partner.description }}</p>
+          </h2>
+          <v-markdown v-if="!!partner.description" :source="partner.description" />
+        </div>
       </li>
     </ul>
 
-    <ul class="communications-list">
+    <!-- <ul class="communications-list">
       <li v-for="communicationPartner in $t('partner.communication')" :key="communicationPartner.name">
         <h2>
           <a
@@ -32,13 +40,20 @@
         </h2>
         <p>{{ communicationPartner.description }}</p>
       </li>
-    </ul>
+    </ul> -->
   </section>
 </template>
 
 <script>
   export default {
-    name: 'Partner'
+    name: 'Partner',
+
+    methods: {
+      getPartnerLogo (token) {
+        const name = token.toLowerCase().replace(/\s/g, '-')
+        return require(`@/assets/${name}-logo.png`)
+      }
+    }
   }
 </script>
 
@@ -46,30 +61,65 @@
   @import "~@/styles/colors";
   @import "~@/styles/layout";
 
+  $breakpoint: 630px;
+
   .partner-page {
     h1 {
       margin-bottom: $base-gap;
     }
   }
 
-  .partners-list {
+  .partners-list,
+  .communications-list {
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
-    justify-content: flex-start;
+    justify-content: center;
     list-style: none;
     width: 100%;
     margin-top: $base-gap;
 
-    & li {
-      width: calc(25% - #{$base-gap});
-      flex: 1 0 auto;
-      border: 1px solid red;
+    li {
+      width: calc(50% - #{$base-gap});
+      flex: 0 0 auto;
+      background: $medium-blue;
+      border-radius: $border-radius-medium;
       margin-bottom: $base-gap;
 
-      &:not(:nth-of-type(4n)) {
+      &:not(:nth-of-type(even)):not(:last-child) {
         margin-right: $base-gap;
       }
+
+      img {
+        width: 100%;
+      }
+
+      @media (max-width: $breakpoint) {
+        margin-right: 0;
+        flex: 0 0 100%;
+        width: 100%;
+
+        &:not(:nth-of-type(even)):not(:last-child) {
+          margin-right: 0;
+        }
+      }
+    }
+
+    h2 {
+      color: $text-color-base;
+      margin-bottom: calc(#{$base-gap} / 2);
+    }
+
+    .partners-list-logo {
+      background: $background-secondary;
+      border-radius: $border-radius-medium;
+      padding: $base-gap;
+      margin-bottom: calc(#{$base-gap} / 2);
+      display: block;
+    }
+
+    .partners-list-content {
+      padding: 0 $base-gap $base-gap;
     }
   }
 </style>
